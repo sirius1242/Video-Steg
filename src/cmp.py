2ed8ef1c315edf936170e96202b83024834aa002
sirius <sirius.ustc@gmail.com>
2022-05-17 15:27:06 +0800

    add test script

---
+++ src/cmp.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+cmd = sys.argv[2] + " " + sys.argv[3]
+a = subprocess.check_output(cmd, shell=True)
+with open(sys.argv[1], 'rb') as fp:
+	b = fp.read()
+
+total = min(len(a), len(b))
+same = 0
+for i in range(total):
+	if a[i] == b[i]:
+		same = same+1;
+	#else:
+		#print(chr(a[i])+'->'+chr(b[i]))
+print(str(same)+"/"+str(total-1)+":"+str(same/(total-1)*100)+"%")
